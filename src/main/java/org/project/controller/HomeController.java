d3ad7ed5f49dc8b411f606365c1618f2c10b538b
Kaitlyn Conron <kaitlynconron@cmail.carleton.ca>
2025-10-28 21:23:31 -0400

    Added create, edit, and delete book methods in the controller and updated/added corresponding HTML.

--- src/main/java/org/project/controller/HomeController.java
+++ src/main/java/org/project/controller/HomeController.java
@@ -10,28 +10,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@Autowired
-	private BookRepo bookRepository;
+    @Autowired
+    private BookRepo bookRepository;
 
 
-	//TODO temp probably wanna have a DB with persistence
-	private boolean setup = true;
-	public void setup(){
-		Book book1 = new Book("0000000000001", "Title1", "Author1", "Publisher1", "Description of book 1");
-		Book book2 = new Book("0000000000002", "Title2", "Author2", "Publisher2", "Description of book 2");
-		Book book3 = new Book("0000000000003", "Title3", "Author3", "Publisher3", "Description of book 3");
+    //TODO temp probably wanna have a DB with persistence
+    private boolean setup = true;
+    public void setup(){
+        Book book1 = new Book("0000000000001", "Title1", "Author1", "Publisher1", "Description of book 1");
+        Book book2 = new Book("0000000000002", "Title2", "Author2", "Publisher2", "Description of book 2");
+        Book book3 = new Book("0000000000003", "Title3", "Author3", "Publisher3", "Description of book 3");
 
-		bookRepository.save(book1);
-		bookRepository.save(book2);
-		bookRepository.save(book3);
-	}
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+        bookRepository.save(book3);
+    }
 
-	@GetMapping("/")
-	public String home(Model model){
-		if (setup){
-			setup = false;
-			setup();
-		}
-		return "home";
-	}
+    @GetMapping("/")
+    public String home(Model model){
+        if (setup){
+            setup = false;
+            setup();
+        }
+        return "home";
+    }
 }
